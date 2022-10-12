0abee707ccb8fde0259acfa83b303ddc6627a02b
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-12 10:49:47 +0200

    zad.cs

---
+++ zadania.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*float n;
+            float silni = 1;
+            n = float.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                silni *= i;
+            }
+            Console.WriteLine(silni);*/
+            /* zad. 5
+            for (int i = -7; i <=17; i=i+4)
+            {
+                Console.WriteLine($"{i}, ");
+            }*/
+
+            /*for (int i=2; i<=486;i=i*3)
+            {
+                Console.WriteLine($"{i}");
+            }*/
+            /*int x = -7;
+            while (x <= 17)
+            {
+                Console.WriteLine(x);
+                x += 4;*/
+            /*int n;
+            n = int.Parse(Console.ReadLine());
+            int w = 0;
+            while (n > 0)
+            {
+                n = n / 10;
+                w = w + 1;
+     
+            }
+            Console.WriteLine(w);*/
+            /*int n;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 2; i < n; i++)
+            {
+                
+                if (n%i==0) { Console.WriteLine(n%i +" nie"); }
+                else { Console.WriteLine(n%i + " tak"); }
+                break;
+            }*/
+
+            /*int n;
+            n = int.Parse(Console.ReadLine());
+            bool flaga = true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    flaga = false;
+                    break;
+                }
+            }
+            if (flaga) Console.WriteLine("jest");
+            else Console.WriteLine("nie jest");
+            Console.ReadKey();
+            */
+        }
+    }
+}
