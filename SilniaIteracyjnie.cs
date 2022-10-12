b2aa6d92eb0a56c2b610ac7747278c5eaecc1f53
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-12 12:12:54 +0200

    Add files via upload

---
+++ SilniaIteracyjnie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static int Silnia(int n) 
+        {
+            /*if (n == 0)
+            {
+                return 1;
+            }
+            return n * Silnia(n - 1);*/
+            int wynik = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                wynik *= i;
+            }
+            return wynik;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Silnia(n));
+        }
+    }
+}
