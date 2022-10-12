72b072a812c9b3af699e9f511c15e185bc30139c
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-12 10:58:06 +0200

    Add files via upload

---
+++ k2zd6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static bool kp2z6(int a, int p)
+        {
+            return (Math.Pow(a, p)-a)%p==0 ? true:false;
+        }
+
+        static void Main(string[] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            if (kp2z6(a, p) == false) { Console.WriteLine($"{p} nie jest liczbą pierwszą"); }
+            else { Console.WriteLine("jest"); }
+        }
+    }
+}
