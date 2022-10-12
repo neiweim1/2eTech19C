13d2eda08aee020933675a7e94c4e427b05e63da
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-12 12:18:46 +0200

    Add files via upload

---
+++ Fibo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static int Fibo(int n) 
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n > 2)
+            {
+                return 1;
+            }
+            return Fibo(n - 1)+Fibo(n-2);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibo(n));
+        }
+    }
+}
