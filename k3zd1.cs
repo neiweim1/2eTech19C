fb489525960b3dc6cb238760a5bb3a389b0d25df
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-12 11:12:55 +0200

    Add files via upload

---
+++ k3zd1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static bool kp3z1(n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i * i * i + 3 +" ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            kp3z1(n);
+        }
+    }
+}
---
+++ k3zd3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        /* public static bool kp3z1(n)
+         {
+             for (int i = 0; i < n; i++)
+             {
+                 Console.WriteLine(i * i * i + 3 +" ");
+             }
+         }
+
+         static void Main(string[] args)
+         {
+             int n = int.Parse(Console.ReadLine());
+             kp3z1(n);
+         }*/
+        public static ArrayList kp3z3(int x)
+        {
+            ArrayList alist = new ArrayList();
+            for (int i = 0; i <x+1; i++)
+            {
+                if (x%i == 0)
+                {
+                    alist.Add(i);
+                }
+            }
+            return alist;
+        }
+        static void Main(string[] args)
+        {
+            ArrayList dzielniki = new ArrayList();
+            dzielniki = kp3z3(12);
+            foreach (int a in dzielniki)
+            {
+                Console.Write(a+" ")
+            }
+        }
+
+    }
+}
