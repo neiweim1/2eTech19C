5454a12be0909c1759ffd21f20f4870767480eb9
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-19 11:53:59 +0200

    Creata KartaPracy3a.cs

---
+++ KartaPracy3a.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+    static void Main(string[] args) 
+    { //zad 1
+        int n;
+        Console.WriteLine("podaj n: ");
+        n = int.Parse(Console.ReadLine());
+        for (int i=1; i<=n; i++){
+              Console.Write("*-|");
+        }
+        
+    }
+}
