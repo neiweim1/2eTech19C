0dabe5f9e6938d158a34ac106d2cfec4313aadac
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-19 11:59:21 +0200

    Update KartaPracy3a.cs

--- KartaPracy3a.cs
+++ KartaPracy3a.cs
@@ -2,12 +2,20 @@ using System;
 
 class Program {
     static void Main(string[] args) 
-    { //zad 1
+    { /*zad 1
         int n;
         Console.WriteLine("podaj n: ");
         n = int.Parse(Console.ReadLine());
         for (int i=1; i<=n; i++){
               Console.Write("*-|");
+        }
+        */
+      //tabliczka mnożenia
+        for (int i=1; i<11; i++){
+              for (int j=1; j<=11; j++){
+                Console.Write(i*j+"\t");
+              }
+          Console.WriteLine();
         }
         
     }
