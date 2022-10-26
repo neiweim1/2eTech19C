444f006dc3625de1e2f196a20ed8042d796f84a8
4GH <115621286+neiweim1@users.noreply.github.com>
2022-10-26 11:15:23 +0200

    Update KartaPracy3a.cs

--- KartaPracy3a.cs
+++ KartaPracy3a.cs
@@ -1,8 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program {
-    static void Main(string[] args) 
-    { /*zad 1
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*zad 1
         int n;
         Console.WriteLine("podaj n: ");
         n = int.Parse(Console.ReadLine());
@@ -10,21 +18,62 @@ class Program {
               Console.Write("*-|");
         }
         */
-      /*tabliczka mnożenia
-        for (int i=1; i<11; i++){
-              for (int j=1; j<=11; j++){
-                Console.Write(i*j+"\t");
-              }
-          Console.WriteLine();*/
-      //gwiazdki
-       int n = 0;
-        for (int i=5; i>n; i--){
-              for (int j=1; j<i+1; j++){
-                Console.Write("*"+"\t");
-              }
-          Console.WriteLine();
+            /*tabliczka mnożenia
+              for (int i=1; i<11; i++){
+                    for (int j=1; j<=11; j++){
+                      Console.Write(i*j+"\t");
+                    }
+                Console.WriteLine();*/
+            //gwiazdki
+            /*
+            int n = 0;
+            for (int i = 5; i > n; i--)
+            {
+                for (int j = 1; j < i + 1; j++)
+                {
+                    Console.Write("*" + "\t");
+                }
+                Console.WriteLine();
+            }
+            */
+            //zad2
+            /*
+            int n;
+            Console.WriteLine("podaj n:");
+            n =int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("∗|| ∗ ∗ − − ∗ ∗ ∗");
+            }
+            */
+            //trojkaty
+            /*
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    if (n - i + 1 >= j) Console.WriteLine("*");
+                    else Console.WriteLine(" ");
+                }
+                
+            }
+            Console.WriteLine();
+            */
+            //zad7
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    /*if  Console.WriteLine("*");
+                    else Console.WriteLine(" ");*/
+                    if (i == 1 || j == 1 || j == n || i == n || (i == n/2+1 && j==n/2+1)) Console.Write("*");
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            
         }
-        
-        
     }
 }
