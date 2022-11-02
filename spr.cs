a82863975b9136008591e8667b6c2aa65d6723b9
4GH <115621286+neiweim1@users.noreply.github.com>
2022-11-02 12:21:36 +0100

    create spr.cs

---
+++ spr.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //znajdz sume ciagu 
+            // 3+6+12+24
+            /*
+            int n = int.Parse(Console.ReadLine());
+            int suma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                suma += 3 * (int)Math.Pow(2,i);
+            }
+            Console.WriteLine(suma);
+            */
+
+            //1+4+7+10
+        }
+    }
+}
