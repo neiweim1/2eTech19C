a46bd0d2e528abeef7d12a3b54830848f2a479f4
4GH <115621286+neiweim1@users.noreply.github.com>
2022-11-09 11:58:29 +0100

    Create sprawdzian.cs

---
+++ sprawdzian.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            //zad1
+            int p = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            //a
+            if ((s/2)*5 >= k)
+            {
+                Console.WriteLine("a) TAK");
+            }
+            else Console.WriteLine("a) NIE");
+            //b
+            if (k > s)
+            {
+                Console.WriteLine("b) "+s / 5);
+            }
+
+            //c
+            if (k == s * 5)
+            {
+                Console.WriteLine("Tak, " + s * 5);
+            }
+            */
+
+        }
+    }
+}
