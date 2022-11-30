521cc8af0dc2dc493b884146e3b9ec2ebe2b2db1
4GH <115621286+neiweim1@users.noreply.github.com>
2022-11-30 12:19:39 +0100

    Create tak.cs

---
+++ tak.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Account
+    {
+        private string emailAdress;
+        private string name;
+
+        public string getEmailAdress()
+        {
+            return "mail";
+        }
+
+        public string getName()
+        {
+            return "name";
+        }
+
+        public void setEmailAdress()
+        {
+            getEmailAdress();
+        }
+
+        public void setName()
+        {
+            getName();
+        }
+
+    }
+
+    class Contact 
+    {
+        private string emailAdress;
+        private string faxNumber;
+        private string name;
+        private string primaryContactMethod;
+
+        public void getEmailAdress()
+        {
+           
+        }
+        public void getFaxNumber()
+        {
+
+        }
+
+        public void getPrimaryContactMethod()
+        {
+
+        }
+
+        public void setEmailAdress()
+        {
+
+        }
+
+        public void setFaxNumber()
+        {
+
+        }
+
+        public void setName()
+        {
+
+        }
+        public void setPrimaryContactMethod()
+        {
+
+        }
+
+    }
+
+    class AdressBook
+    {
+        private string name;
+
+        public void getContact()
+        {
+
+        }
+
+        public void getContacts()
+        {
+
+        }
+
+        public void getName()
+        {
+            this.name
+        }
+
+        public void insertContact()
+        {
+
+        }
+
+        public void setName()
+        {
+
+        }
+    }
+
+    class ContactGroup
+    {
+        private string name;
+        public void getName()
+        {
+
+        }
+
+        public void setName()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               
+        }
+    }
+}
