2b6fc409369b79a2a8fa2adbd0aa1279185e9049
4GH <115621286+neiweim1@users.noreply.github.com>
2022-12-14 12:01:50 +0100

    Create kartapracy6.cs

---
+++ kartapracy6.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+
+    internal class Program
+    {
+        public static bool artymetyczny(int a, int b, int c)
+        {
+            if ((c-b == b - a)||(a-b==b-c)) return true;
+            else return false;
+        }
+
+        public static bool geometryczny(int a, int b, int c)
+        {
+            if (((b/a) * b == c) || (c*(a/b)==b)) return true;
+            else return false;
+
+        }
+
+        public static bool rosnie(int a, int b, int c)
+        {
+            if (a<b && b<c) return true;
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int[] tab = new int[3];
+            for(int i= 0; i < 3; i++)
+            {
+                tab[i] = int.Parse(Console.ReadLine());
+            }
+            if (rosnie(tab[0], tab[1], tab[2])) Console.WriteLine("rosnący");
+            else Console.WriteLine("malejący");
+            if (artymetyczny(tab[0], tab[1], tab[2])) Console.WriteLine("artymetyczny ciąg");
+            if (geometryczny(tab[0], tab[1], tab[2])) Console.WriteLine("geometryczny ciąg");
+
+
+        }
+
+    }
+}
