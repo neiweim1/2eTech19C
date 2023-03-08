b073c2389089a5afbb6c11f9b47c630b55c54555
4GH <115621286+neiweim1@users.noreply.github.com>
2023-03-08 12:08:53 +0100

    Update spr3.cs

--- spr3.cs
+++ spr3.cs
@@ -11,10 +11,19 @@ namespace ConsoleApp1
     {
         public string Marka { get; set; }
         public string Model { get; set; }
+        public virtual void Wyswietl()
+        {
+            Console.WriteLine($"Marka = {Marka}, Model = {Model}");
+        }
     }
     class PC : Komputer
     {
         public string Rodzaj_obudowy { get; set; }
+        public override void Wyswietl()
+        {
+            base.Wyswietl();
+            Console.WriteLine($"Rodzaj obudowy = {Rodzaj_obudowy}");
+        }
     }
 
     class Laptop : Komputer
@@ -23,6 +32,12 @@ namespace ConsoleApp1
         private string kolor_obudowy;
         public string getKolor_obudowy() { return kolor_obudowy; } //ten dluzszy get
         public void setKolor_obudowy(string kolor) { this.kolor_obudowy = kolor; } //ten dluzszy set
+
+        public override void Wyswietl()
+        {
+            base.Wyswietl();
+            Console.WriteLine($"Długość przekątnej = {Dlugosc_przekatnej} , Kolor obudowy = {kolor_obudowy}");
+        }
     }
 
     class Osoba
@@ -60,12 +75,23 @@ namespace ConsoleApp1
             this.akomputer = akomputer;
             this.aklient = aklient;
             this.aopiekun = aopiekun;
-        }   
+        }
+
+        
+        public Komputer GetKomputer() { return akomputer; }
+        public void SetKomputer(Komputer akomputer) { this.akomputer = akomputer; }
+
+        public Klient GetKlient() { return aklient; }
+        public void SetKlient(Klient aklient) { this.aklient = aklient; }
+
+        public Opiekun GetOpiekun() { return aopiekun; }
+        public void SetOpiekun(Opiekun aopiekun) { this.aopiekun = aopiekun; }
+        //kazdego get/set mozna uzyc na spr
     }
 
     class Naprawa
     {
-        Zlecenie azlecenie { get; }
+        Zlecenie azlecenie { get; set; }
         Serwisant aserwisant { get; set; }
         public void Setzlecenie(Zlecenie azlecenie)
         {
@@ -73,10 +99,55 @@ namespace ConsoleApp1
         }
     }
 
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            //proste banalne
+            /*
+            Laptop lap = new Laptop();
+            lap.Marka = Console.ReadLine();
+            lap.Model = Console.ReadLine();
+            lap.Dlugosc_przekatnej = float.Parse(Console.ReadLine());
+            lap.setKolor_obudowy(Console.ReadLine());
+
+            Console.WriteLine($"{lap.Marka} + {lap.Model} + {lap.Dlugosc_przekatnej} + {lap.getKolor_obudowy()}"); //zapis jest zalezny od tego jakiego setera/getera uzyles ostatni to ten dluzszy np
+            */
+            //hard and creative
+            bool b = true;
+            while (b) {
+
+
+                Console.Write("Co masz do naprawy? Laptop czy kloc?/ exit e");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "laptop":
+                        Laptop lap = new Laptop();
+                        lap.Marka = Console.ReadLine();
+                        lap.Model = Console.ReadLine();
+                        lap.Dlugosc_przekatnej = float.Parse(Console.ReadLine());
+                        lap.setKolor_obudowy(Console.ReadLine());
+                        lap.Wyswietl();
+                        break;
+                    case "komputer":
+                        PC k = new PC();
+                        k.Marka = Console.ReadLine();
+                        k.Model = Console.ReadLine();
+                        k.Rodzaj_obudowy = Console.ReadLine();
+                        k.Wyswietl();
+                        break;
+                    case "e":
+                        b = false;
+                        break;
+                    default: Console.WriteLine("błędna opcja, albo komputer albo laptop");
+                        break;
+                }
+            }
+
         }
     }
 }
