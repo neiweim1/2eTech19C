e164d8409665cfceacece71a217a6e17900a6cb7
4GH <115621286+neiweim1@users.noreply.github.com>
2023-09-13 08:39:01 +0200

    Update wpf.cs

--- wpf.cs
+++ wpf.cs
@@ -16,6 +16,38 @@ using System.Windows.Shapes;
 
 namespace WpfApp5
 {
+    /*
+        <Window x:Class="WpfApp5.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:WpfApp5"
+        mc:Ignorable="d"
+        Title="MainWindow" Height="450" Width="800">
+    <Grid>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="2*"/>
+            <RowDefinition Height="29*"/>
+        </Grid.RowDefinitions>
+        <TextBox  TextWrapping="Wrap" Margin="359,28,374,385" Grid.RowSpan="2" Background="#FFFFBD6D" x:Name="name"/>
+        <CheckBox Content="kotki &lt;3" Margin="364,125,331,261" Grid.Row="1" x:Name="check1"/>
+        <CheckBox Content="gry" Margin="364,150,342,231"  Grid.Row="1"  x:Name="check2"/>
+        <RadioButton Content="woman haha" Margin="374,52,335,338" Grid.Row="1" x:Name="gender" IsChecked="False"/>
+        <RadioButton Content="men" Margin="374,73,374,317" Grid.Row="1" x:Name="gender1"/>
+        <TextBox Name="textarea"
+            TextWrapping="Wrap"
+            AcceptsReturn="True"
+             VerticalScrollBarVisibility="Visible" Margin="203,213,223,20" Background="#FFFFAEAE"  Grid.Row="1">
+
+        </TextBox>
+        <Label Content="plec" Margin="263,47,438,333" Grid.Row="1" Background="#FFFFBEBE"/>
+        <Label Content="hobby" Margin="263,96,438,284" Grid.Row="1" Background="#FFFFBEBE"/>
+        <Button Content="Button" Margin="71,358,661,20" Grid.Row="1" Click="Button_Click"/>
+    </Grid>
+</Window>
+
+    */
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
