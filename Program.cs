f1aed9c37dc72352cbdd4dd346a605dbc7f6baf6
4GH <115621286+neiweim1@users.noreply.github.com>
2024-03-11 09:32:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Finanse
+    {
+        public int Cena {  get; set; }
+        private Random rand = new Random();
+        double wartosc;
+        public double Rabatuje10(int Cena)
+        {
+            return Cena*0.90;
+
+        }
+
+        public double RabatujeLosowy(int Cena, double wartosc)
+        {
+            return (1 - (wartosc / 100d))*Cena;
+
+        }
+
+        public double Marzuje20(int Cena)
+        {
+            wartosc = 0.20;
+            return (1-wartosc)*Cena;
+
+        }
+
+        public double MarzujeLosowy(int Cena, double wartosc)
+        {
+            return (1 + (wartosc / 100d)) * Cena;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
